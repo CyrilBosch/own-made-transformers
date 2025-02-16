e50b418bf4dfbd2e8e47b80c7b7d5334d9638910
Halin Cyril Yvon Jacques (BEG/EOR2) <CyrilYvonJacques.Halin@de.bosch.com>
2025-02-16 12:36:21 +0100

    feat: LayerNormalization added

--- src/model.py
+++ src/model.py
@@ -35,3 +35,15 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False)
         return self.dropout(x)
+    
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 1e-6):
+        super._init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim = True)
+        std = x.std(dim = -1, keepdim = True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
